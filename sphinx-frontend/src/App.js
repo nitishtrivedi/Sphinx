18694f118a352bf18348e1214da84506016edef0
nitishtrivedi <nitish_trivedi0194@yahoo.com>
2022-10-09 14:14:19 +0530

    Commit 1 - Create React App

---
+++ sphinx-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Sphinx</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
